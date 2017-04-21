d60fed177a61894b8b71e6c70a8ea5b6196d1638
caophivo <caophivo1991@gmail.com>
2017-04-20 20:39:02 -0700

    Load data the admin page product list

---
+++ OnlineShop/Areas/Admin/BUS/ProductBUS.cs
@@ -0,0 +1,23 @@
+﻿using ASP_OnlineShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.BUS
+{
+    public class ProductBUS
+    {
+        public static IEnumerable<SanPham> List()
+        {
+            var db = new ASP_OnlineShopConnectionDB();
+            return db.Query<SanPham>("SELECT * FROM SanPham");
+        }
+
+        public static SanPham ProductDetail(int id)
+        {
+            var db = new ASP_OnlineShopConnectionDB();
+            return db.SingleOrDefault<SanPham>("select * from SanPham where MaSanPham = @0", id);
+        }
+    }
+}
