86aa04307fd8981cdba73370a3099377599bb30a
caophivo <caophivo1991@gmail.com>
2017-04-22 11:29:43 -0700

    load menu loai san pham

---
+++ OnlineShop/BUS/ProductTypeBUS.cs
@@ -0,0 +1,19 @@
+﻿using ASP_OnlineShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.BUS
+{
+    public class ProductTypeBUS
+    {
+        public static IEnumerable<LoaiSanPham> ListProductType()
+        {
+            using (var db = new ASP_OnlineShopConnectionDB())
+            {
+                return db.Query<LoaiSanPham>("select * from LoaiSanPham where DaXoa=0");
+            }
+        }
+    }
+}
---
+++ OnlineShop/Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.BUS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult ProductType()
+        {
+            return View(ProductTypeBUS.ListProductType());
+        }
+    }
+}
